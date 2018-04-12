b8106858f2ebe82c9d6aef12c3888163e746bfdd
naren <blazeraze.naren@gmail.com>
2018-04-12 22:38:39 +0300

    REST end points with https redirection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
-
+import { CatComponent } from './cat/cat.component';
+import { CatCreateComponent } from './cat-create/cat-create.component';
+import { CatEditComponent } from './cat-edit/cat-edit.component';
+import { CatService } from './cat.service';
+import { AppRoutingModule } from './app-routing.module';
+import { FilterPipe } from './filter.pipe';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatComponent,
+    CatCreateComponent,
+    CatEditComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, FormControl, HttpClientModule, ReactiveFormsModule, AppRoutingModule
   ],
-  providers: [],
+  providers: [CatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
